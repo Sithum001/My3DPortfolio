e5ab008e5ec6b6e54131a8be69b8a9ecf6a11c3e
Sithum001 <128293308+Sithum001@users.noreply.github.com>
2025-11-10 23:45:15 +0530

    logo section

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,19 @@ import React from 'react'
 import Hero from './sections/Hero'
 import './App.css'
 import ShowcaseSection from './sections/ShowcaseSection'
+import Navbar from './components/Navbar'
+import LogoSection from './sections/LogoSection'
+import FeatureCards from './sections/FeatureCards'
 
 
 const App = () => {
   return (
     <>
+    <Navbar/>
     <Hero/>
     <ShowcaseSection/>
+    <LogoSection/>
+    <FeatureCards/>
     </>
   )
 }
--- src/sections/ShowcaseSection.jsx
+++ src/sections/ShowcaseSection.jsx
@@ -1,12 +1,55 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import {gsap} from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { useGSAP } from '@gsap/react';
+
+gsap.registerPlugin(ScrollTrigger);
 
 const ShowcaseSection = () => {
+const sectionRef =useRef(null);
+const project1Ref=useRef(null);
+const project2Ref=useRef(null);
+const project3Ref=useRef(null);
+
+const projects =[project1Ref.current,project2Ref.current,project3Ref.current];
+
+
+
+useGSAP(()=>{
+const projects =[project1Ref.current,project2Ref.current,project3Ref.current];
+
+
+
+    projects.forEach((card,index)=>{
+    gsap.fromTo(
+        card,
+        {
+            y:50,opacity:0
+        },
+        {
+            y:0,
+            opacity:1,
+            duration:1,
+            delay:0.6 *(index + 1),
+            scrollTrigger:{
+                trigger:card,
+                start:'top bottom-=100',
+            }
+        }
+    )
+})
+gsap.fromTo(
+    sectionRef.current,
+    {opacity:0},
+    {opacity:1,duration:1.5}
+)
+},[]);
   return (
-    <div id="work" className="app-showcase">
+    <div id="work" ref={sectionRef} className="app-showcase">
         <div className='w-full'>
             <div className='showcaselayout'>
                 {/* left*/}
-                <div className='first-project-wrapper'>
+                <div className='first-project-wrapper' ref={project1Ref} >
                     <div className='image-wrapper'>
                         <img src="/images/project1.png" alt="Ryde" />
                     </div>
@@ -17,14 +60,14 @@ const ShowcaseSection = () => {
                 </div>
 
                 {/*right */}
-                  <div className='project-list-wrapper overflow-hidden'>
-                    <div className='project'>
+                  <div className='project-list-wrapper overflow-hidden' ref={project2Ref}>
+                    <div className='project '>
                         <div className='image-wrapper bg-[#ffefdb]'>
                             <img src="/images/project2.png" alt="Library Management Platform" />
                         </div>
                         <h2>Library Management Platform</h2>
                     </div>
-                    <div className='project'>
+                    <div className='project' ref={project3Ref}>
                         <div className='image-wrapper bg-[#ffe7eb]'>
                             <img src="/images/project3.png" alt="YC Derectory" />
                         </div>
